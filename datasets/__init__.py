89fb0efb73266c43fb5601727b28ed4c62f39dae
Yifan Chen <yifanchen98@hotmail.com>
2025-04-28 22:21:26 +0200

    added preprocess_data and set_prior_grn

---
+++ datasets/__init__.py
@@ -0,0 +1,11 @@
+from ._datasets import (
+    zebrafish_nc,
+    zebrafish_grn,
+    murine_nc,
+)
+
+__all__ = [
+    "zebrafish_nc",
+    "zebrafish_grn",
+    "murine_nc",
+]
---
+++ preprocessing/preprocess_data.py
@@ -0,0 +1,56 @@
+# the folloing code is adapted from velovi
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+import scvelo as scv
+from anndata import AnnData
+from sklearn.preprocessing import MinMaxScaler
+
+
+def preprocess_data(
+    adata: AnnData,
+    spliced_layer: Optional[str] = "Ms",
+    unspliced_layer: Optional[str] = "Mu",
+    min_max_scale: bool = True,
+    filter_on_r2: bool = True,
+) -> AnnData:
+    """Preprocess data.
+
+    This function removes poorly detected genes and minmax scales the data.
+
+    Parameters
+    ----------
+    adata
+        Annotated data matrix.
+    spliced_layer
+        Name of the spliced layer.
+    unspliced_layer
+        Name of the unspliced layer.
+    min_max_scale
+        Min-max scale spliced and unspliced
+    filter_on_r2
+        Filter out genes according to linear regression fit
+
+    Returns
+    -------
+    Preprocessed adata.
+    """
+    if min_max_scale:
+        scaler = MinMaxScaler()
+        adata.layers[spliced_layer] = scaler.fit_transform(adata.layers[spliced_layer])
+
+        scaler = MinMaxScaler()
+        adata.layers[unspliced_layer] = scaler.fit_transform(
+            adata.layers[unspliced_layer]
+        )
+
+    if filter_on_r2:
+        scv.tl.velocity(adata, mode="deterministic")
+
+        adata = adata[
+            :, np.logical_and(adata.var.velocity_r2 > 0, adata.var.velocity_gamma > 0)
+        ].copy()
+        adata = adata[:, adata.var.velocity_genes].copy()
+
+    return adata
