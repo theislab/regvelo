8467aaa88875ca039b24eac9eaaf568e1d938d1d
emmaaer <80543771+emmaaer@users.noreply.github.com>
2026-07-08 21:37:21 +0200

    Add new plotting functions to __init__.py

--- src/regvelo/plotting/__init__.py
+++ src/regvelo/plotting/__init__.py
@@ -5,11 +5,18 @@ from ._cellfate_perturbation import cellfate_perturbation
 from ._simulated_visit_diff import simulated_visit_diff
 from ._regulatory_network import regulatory_network
 from ._depletion_score import depletion_score
+from ._markov_screen import _plot_visits_dist_combined
+from ._driver_TF_ranking import plot_top_TF, compute_TF_regulon, plot_grn_weight, plot_GRN_per_TF
 
 __all__ = [
         "commitment_score",
         "cellfate_perturbation",
         "simulated_visit_diff",
         "regulatory_network",
-        "depletion_score"
+        "depletion_score",
+        "_plot_visits_dist_combined",
+        "plot_top_TF",
+        "compute_TF_regulon",
+        "plot_grn_weight",
+        "plot_GRN_per_TF",
         ]
