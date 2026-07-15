201d086a4bc83744508bb24d435fe016c7003ba8
emmaaer <80543771+emmaaer@users.noreply.github.com>
2026-07-15 11:58:50 +0200

    Refactor imports and update __all__ in __init__.py

--- src/regvelo/plotting/__init__.py
+++ src/regvelo/plotting/__init__.py
@@ -5,8 +5,11 @@ from ._cellfate_perturbation import cellfate_perturbation
 from ._simulated_visit_diff import simulated_visit_diff
 from ._regulatory_network import regulatory_network
 from ._depletion_score import depletion_score
-from ._markov_screen import _visits_diff_per_tf, _plot_visits_dist, _plot_visits_dist_combined
-from ._driver_TF_ranking import plot_top_TF, plot_grn_weight, plot_GRN_per_TF
+from ._visits_diff_per_tf import visits_diff_per_tf
+from ._plot_visits_dist import plot_visits_dist
+from ._plot_visits_dist_combined import plot_visits_dist_combined
+from ._driver_TF_ranking import plot_top_TF 
+from ._plot_TF_regulon import plot_grn_weight, plot_regulon, plot_GRN_per_TF
 
 __all__ = [
         "commitment_score",
@@ -14,10 +17,11 @@ __all__ = [
         "simulated_visit_diff",
         "regulatory_network",
         "depletion_score",
-        "_visits_diff_per_tf",
-        "_plot_visits_dist",
-        "_plot_visits_dist_combined",
+        "visits_diff_per_tf",
+        "plot_visits_dist",
+        "plot_visits_dist_combined",
         "plot_top_TF",
         "plot_grn_weight",
+        "plot_regulon",
         "plot_GRN_per_TF",
         ]
